e81ee35b20be3507877a2a5c9e43b4bce9e930ab
Jonah Botelho <jbotelho3@my.bcit.ca>
2026-03-17 14:38:42 -0700

    chore: add commends explaining what each heuristic weight represents

--- abalone/players/teams/abdullah/heuristic.py
+++ abalone/players/teams/abdullah/heuristic.py
@@ -4,16 +4,16 @@ from ....ai.heuristics import build_weighted_evaluator
 
 # Starts from the shared baseline. Abdullah can tune only this file later.
 ABDULLAH_WEIGHTS = {
-    "marble": 50000.0,
-    "center": 40.0,
-    "cohesion": 35.0,
-    "cluster": 30.0,
-    "edge": 60.0,
-    "formation": 50.0,
-    "push": 120.0,
-    "threat": 80.0,
-    "mobility": 20.0,
-    "stability": 25.0,
+    "marble": 50000.0,    # Material advantage (number of marbles)
+    "center": 40.0,       # Distance to the center of the board
+    "cohesion": 35.0,     # Grouping of marbles together
+    "cluster": 30.0,      # Number of contiguous marble groups
+    "edge": 60.0,         # Distance from the edge of the board
+    "formation": 50.0,    # Maintaining strong defensive shapes
+    "push": 120.0,        # Ability to push opponent marbles
+    "threat": 80.0,       # Direct threats to push opponent off board
+    "mobility": 20.0,     # Number of available legal moves
+    "stability": 25.0,    # How safe marbles are from being pushed
 }
 
 evaluate_abdullah = build_weighted_evaluator(ABDULLAH_WEIGHTS)
