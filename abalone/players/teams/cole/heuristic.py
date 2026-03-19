c20d51e9879cd36da7a7a6b4cdd63715058f7951
Cole Campbell <ccampbell211@my.bcit.ca>
2026-03-18 20:33:39 -0400

    edited cole heuristics, the best i have so far

--- abalone/players/teams/cole/heuristic.py
+++ abalone/players/teams/cole/heuristic.py
@@ -4,16 +4,17 @@ from ....ai.heuristics import build_weighted_evaluator
 
 # Starts from the shared baseline. Cole can tune only this file later.
 COLE_WEIGHTS = {
-    "marble": 50000.0,    # Material advantage (number of marbles)
-    "center": 40.0,       # Distance to the center of the board
-    "cohesion": 35.0,     # Grouping of marbles together
-    "cluster": 30.0,      # Number of contiguous marble groups
-    "edge": 60.0,         # Distance from the edge of the board
-    "formation": 50.0,    # Maintaining strong defensive shapes
-    "push": 120.0,        # Ability to push opponent marbles
-    "threat": 80.0,       # Direct threats to push opponent off board
-    "mobility": 20.0,     # Number of available legal moves
-    "stability": 25.0,    # How safe marbles are from being pushed
+    "marble": 50000.0,  # Material advantage (number of marbles)
+    "center": 500.0,    # Distance to the center of the board
+    "cohesion": 100.0,  # Grouping of marbles together
+    "cluster": 30.0,    # Number of contiguous marble groups
+    "edge": 60.0,       # Distance from the edge of the board
+    "formation": 50.0,  # Maintaining strong defensive shapes
+    "push": 120.0,      # Ability to push opponent marbles
+    "threat": 100.0,    # Direct threats to push opponent off board
+    "mobility": 20.0,       # Number of available legal moves
+    "stability": 30.0,  # How safe marbles are from being pushed
 }
+# TODO game clock still adding time after every turn
 
 evaluate_cole = build_weighted_evaluator(COLE_WEIGHTS)
