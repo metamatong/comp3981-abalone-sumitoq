7eba35dbc1dabe54771b524322157cde7b0e4930
Jonah Botelho <jbotelho3@my.bcit.ca>
2026-03-26 10:31:45 -0700

    tune: update jonah heuristics

--- abalone/players/teams/jonah/heuristic.py
+++ abalone/players/teams/jonah/heuristic.py
@@ -4,8 +4,8 @@ from ....ai.heuristics import build_weighted_evaluator
 
 # Starts from the shared baseline. Jonah can tune only this file later.
 JONAH_WEIGHTS = {
-    "marble": 50000.0,    # Material advantage (number of marbles)
-    "center": 450.0,      # Distance to the center of the board
+    "marble": 40000.0,    # Material advantage (number of marbles)
+    "center": 500.0,      # Distance to the center of the board
     "cohesion": 100.0,    # Grouping of marbles together
     "cluster": 30.0,      # Number of contiguous marble groups
     "edge": 40.0,         # Distance from the edge of the board
@@ -13,7 +13,7 @@ JONAH_WEIGHTS = {
     "push": 120.0,        # Ability to push opponent marbles
     "threat": 100.0,      # Direct threats to push opponent off board
     "mobility": 20.0,     # Number of available legal moves
-    "stability": 30.0,    # How safe marbles are from being pushed
+    "stability": 30.0,    # How safe marbles are from being pushed.
 }
 
 evaluate_jonah = build_weighted_evaluator(JONAH_WEIGHTS)
