027d542fcf726d2607c800513608604f59512191
Cole Campbell <ccampbell211@my.bcit.ca>
2026-03-30 13:07:51 -0700

    changed all tournament agent depths to 3

--- abalone/players/teams/belgian_daisy/agents.py
+++ abalone/players/teams/belgian_daisy/agents.py
@@ -9,7 +9,7 @@ AGENTS = [
         label="Tournament: Belgian Daisy",
         owner="Tournament",
         evaluator=evaluate_belgian_daisy,
-        default_depth=4,
+        default_depth=3,
         tie_break="lexicographic",
     )
 ]
