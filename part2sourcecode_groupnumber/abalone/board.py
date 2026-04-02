737d84a3c20bffadcd7171c533a1ab88a4ff0cd7
Kyle Cheon <kcheon@my.bcit.ca>
2026-04-02 09:05:16 -0700

    chore: remove irrelevant code

--- part2sourcecode_groupnumber/abalone/board.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility shim for board interfaces.
-
-Source of truth lives in abalone.game.board.
-"""
-
-from .game.board import *  # noqa: F401,F403
--- part2sourcecode_groupnumber/abalone/game/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Minimal game package for the Part 2 submission bundle."""
