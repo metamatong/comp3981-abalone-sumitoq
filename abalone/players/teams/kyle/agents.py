fa4ad4cd47adc00a8542b9c25ccda893a52364e2
Kyle Cheon <kcheon@my.bcit.ca>
2026-04-06 01:21:12 -0700

    chore: change quiescence depth for kyle to 2

--- abalone/players/teams/kyle/agents.py
+++ abalone/players/teams/kyle/agents.py
@@ -11,6 +11,6 @@ AGENTS = [
         evaluator=evaluate_kyle,
         default_depth=4,
         tie_break="lexicographic",
-        max_quiescence_depth=3,
+        max_quiescence_depth=2,
     )
 ]
